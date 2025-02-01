e6423c4e9a75e5737bb506a2dd1f1386ffdf2e89
lexil <lexilou_9@msn.com>
2025-02-01 14:47:33 -0500

    Got a working TSH Graph

--- notebooks/Thyroid_Labs/server.R
+++ notebooks/Thyroid_Labs/server.R
@@ -1,17 +1,21 @@
 function(input, output, session) {
   
   filtered_data <- reactive({
-    # Filter out rows where TSH or age is NA
+    
     data <- thyroid_df_shiny[!is.na(thyroid_df_shiny$TSH) & !is.na(thyroid_df_shiny$age), ]
     
-    # Create age groups based on the filtered data
     data$age_group <- cut(data$age,
                           breaks = c(-Inf, 1, 6, 11, 20, 99),
                           labels = c("Babies < 1", "Children 1-6", "Children 7-11", "Ages 12-20", "Adults 21-99"),
                           right = TRUE)
     
-    # Filter the data based on the TSH value input
-    data[abs(data$TSH - input$tsh_value) <= 1, ]
+    data <- data[abs(data$TSH - input$tsh_value) <= 1, ]
+    
+    if (input$sex != "All") {
+      data <- data[data$sex == input$sex, ]
+    }
+    
+    return(data)
   })
   
   output$histogram <- renderPlot({
@@ -23,7 +27,7 @@ function(input, output, session) {
       labs(title = paste("Histogram of TSH Levels Around", input$tsh_value, "Across Different Age Ranges by Sex"),
            x = "TSH Level",
            y = "Frequency") +
-      scale_fill_manual(values = c("pink", "skyblue")) +
+      scale_fill_manual(values = c("F"= "pink", "M"= "skyblue")) +
       theme_minimal()+
       theme(
         plot.title = element_text(size = 20),
@@ -31,7 +35,8 @@ function(input, output, session) {
         axis.text.y = element_text(size= 12),
         axis.text.x = element_text(size= 12),
         plot.margin = margin(t = 30, b = 40),
-        strip.text = element_text(size = 12, margin = margin(t = 10, b = 10))
+        strip.text = element_text(size = 12, margin = margin(t = 10, b = 10)),
+        panel.spacing = unit(2, "lines")
       )
   })
 }
--- notebooks/Thyroid_Labs/ui.R
+++ notebooks/Thyroid_Labs/ui.R
@@ -3,8 +3,19 @@ ui <- fluidPage(
   
   sidebarLayout(
     sidebarPanel(
-      numericInput("tsh_value", "Enter TSH Value:", value = 2, min = 0, max = 530, step = 0.1),
-      p("Use the slider to filter TSH values."),
+      numericInput("tsh_value", 
+                  "Enter TSH Value:",
+                  value = 2,
+                  min = 0, 
+                  max = 530,
+                  step = 0.1),
+      p("Enter a numeric input to filter TSH values"),
+      
+      radioButtons("sex", 
+                   "Select Sex:",
+                   choices = c("All", "M", "F"), 
+                   selected = "All"),
+      p("Use the radio buttons to filter by sex."),
     ),
     
     mainPanel(
