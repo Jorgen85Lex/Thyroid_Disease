9d60d00de24a3f530d7493fe028ee34b05df77e7
lexil <lexilou_9@msn.com>
2025-02-06 20:28:09 -0500

    Working on Free T3 graph generation

--- notebooks/Thyroid_Labs/server.R
+++ notebooks/Thyroid_Labs/server.R
@@ -1,5 +1,5 @@
 server <- function(input, output, session) {
-  
+#TSH age ranges for user input
   reactive_age_range <- reactive({
     age <- input$user_age
     
@@ -45,7 +45,6 @@ server <- function(input, output, session) {
   filtered_data <- reactive({
     data <- thyroid_df_shiny[!is.na(thyroid_df_shiny$TSH) & !is.na(thyroid_df_shiny$age), ]
     
-    # Age groups for TSH levels
     data$age_group <- cut(data$age,
                           breaks = c(-Inf, 1, 6, 11, 20, 99),
                           labels = c("Babies < 1", "Children 1-6", "Children 7-11", "Ages 12-20", "Adults 21-99"),
@@ -68,7 +67,7 @@ server <- function(input, output, session) {
       return(NULL)
     }
     
-
+    
     plot <- ggplot(data, aes(x = TSH, fill = sex)) +
       geom_histogram(binwidth = 1, color = "black", alpha = 0.7, position = "dodge") +
       facet_wrap(~ age_group, nrow = 1) +
@@ -133,7 +132,137 @@ server <- function(input, output, session) {
       user_comparison_message
     })
   })
+  
+  #T3 age ranges for user input
+  reactive_age_range_t3 <- reactive({
+    age_t3 <- input$user_age_t3
+    
+    if (age_t3 <= 1) {
+      return("Babies < 1 year")
+    } else if (age_t3 >= 1 && age_t3 <= 6) {
+      return("Children 1 to 6 years")
+    } else if (age_t3 >= 7 && age_t3 <= 11) {
+      return("Children 7 to 11 years")
+    } else if (age_t3 >= 12 && age_t3 <= 17) {
+      return("Children 12 to 17 years")
+    } else {
+      return("Adults 18 to 99 years")
+    }
+  })
+  
+  observe({
+    selected_range_t3 <- reactive_age_range_t3()
+    
+    updateSelectInput(session, "comparison_age_range_t3", selected = selected_range_t3)
+  })
+  
+  
+  compare_clicked_t3 <- reactiveVal(FALSE)
+  
+  observeEvent(input$compare_button_t3, {
+    req(input$user_age_t3, input$user_t3)
+    compare_clicked_t3(TRUE)
+    print(paste("Button clicked. Free T3 value: ", input$user_t3)) 
+  })
+  
+  #normal Free T3 ranges
+    normal_ranges_t3 <- list(
+      "Babies < 1 year" = c(min = 1.4, max = 6.4),
+      "Children 1 to 6 years" = c(min = 2.0, max = 6.0),
+      "Children 7 to 11 years" = c(min = 2.7, max = 5.2),
+      "Children 12 to 17 years" = c(min = 2.3, max = 5.0),
+      "Adults 18 to 99 years" = c(min = 2.3, max = 4.1)
+    )
+    
+    filtered_data_t3 <- reactive({
+      data_t3 <- thyroid_df_shiny[!is.na(thyroid_df_shiny$T3) & !is.na(thyroid_df_shiny$age), ]
+      
+      data_t3$age_group_t3 <- cut(data_t3$age,
+                            breaks = c(-Inf, 1, 6, 11, 17, 99),
+                            labels = c("Babies < 1", "Children 1-6", "Children 7-11", "Ages 12-17", "Adults 18-99"),
+                            right = TRUE)
+      
+      data_t3 <- data_t3[abs(data_t3$T3 - input$t3_value) <= 1, ]
+      
+      if (input$sex_t3 != "All") {
+        data_t3 <- data_t3[data_t3$sex == input$sex_t3, ]
+      }
+      
+      return(data_t3)
+    }) 
+    
+    output$T3histogram <- renderPlot({
+      data_t3 <- filtered_data_t3() 
+      
+      if (nrow(data_t3) == 0) {
+        return(NULL)
+      }
+      
+      plot <- ggplot(data_t3, aes(x = T3, fill = sex)) +
+        geom_histogram(binwidth = 1, color = "black", alpha = 0.7, position = "dodge") +
+        facet_wrap(~ age_group_t3, nrow = 1) +
+        labs(title = paste("Histogram of T3 Levels Around", input$user_t3, "Across Different Age Ranges by Sex"),
+             x = "T3 Level",
+             y = "Frequency") +
+        scale_fill_manual(values = c("F" = "pink", "M" = "skyblue")) +
+        theme_minimal() +
+        theme(
+          plot.title = element_text(size = 20),
+          axis.title.y = element_text(size = 14),
+          axis.text.y = element_text(size = 12),
+          axis.text.x = element_text(size = 12),
+          plot.margin = margin(t = 30, b = 40),
+          strip.text = element_text(size = 12, margin = margin(t = 10, b = 10)),
+          panel.spacing = unit(2, "lines")
+        )
+      
+      if (compare_clicked_t3()) {
+        plot <- plot + 
+          geom_vline(
+            data = data_t3[data_t3$age_group_t3 == reactive_age_range_t3(), ],
+            aes(xintercept = input$user_t3),
+            color = "red", 
+            linetype = "dashed", 
+            size = 1
+          )
+      }
+      
+      return(plot)
+    })
+    
+    output$no_labs_available_message_t3 <- renderText({
+      data_t3 <- filtered_data_t3()
+      
+      if (nrow(data_t3) == 0) {
+        return("No results matching that input.")
+      } else {
+        return(NULL)
+      }
+    })
+    
+    observeEvent(input$compare_button_t3, {
+      req(input$user_age_t3, input$user_t3)
+      
+      normal_range_t3 <- normal_ranges_t3[[input$comparison_age_range_t3]]
+      normal_min_t3 <- normal_range_t3["min"]
+      normal_max_t3 <- normal_range_t3["max"]
+    
+      normal_status_t3 <- "within range"
+      if (input$user_t3 < normal_min_t3) {
+        normal_status_t3 <- "lower than expected"
+      } else if (input$user_t3 > normal_max_t3) {
+        normal_status_t3 <- "higher than expected"
+      }
+    
+    user_comparison_message_t3 <- paste("Your Free T3 value is ", input$user_t3, ".\n",
+                                     "This is ", normal_status_t3, " for your age group (", input$comparison_age_range_t3, ").\n")
+    
+    output$user_comparison_message_t3 <- renderText({
+      user_comparison_message_t3
+    })
+  })
 }
+
 
 
 
--- notebooks/Thyroid_Labs/ui.R
+++ notebooks/Thyroid_Labs/ui.R
@@ -1,40 +1,88 @@
 ui <- fluidPage(
-  titlePanel("TSH Level Distribution by Age and Sex"),
+  titlePanel("TSH and Free T3 Level Distribution by Age and Sex"),
   
-  sidebarLayout(
-    sidebarPanel(
-      h3("Distribution of TSH Levels"),
-      numericInput("tsh_value", 
-                   "Input TSH Value (in mU/L):", 
-                   value = 2, 
-                   min = 0, 
-                   max = 530, 
-                   step = 0.25),
-      p("Use the slider to view TSH values."),
-      
-      radioButtons("sex", 
-                   "Select Sex for Distribution:",
-                   choices = c("All", "M", "F"), 
-                   selected = "All"),
-      p("Use the buttons to filter by sex in the distribution."),
-      
-      br(),
-      
-      h3("Compare Your TSH Level to the Expected Range"),
-      numericInput("user_age", "Enter your age:", value = 30, min = 0, max = 120),
-      numericInput("user_tsh", "Enter your TSH value:", value = 2, min = 0, max = 530),
-      selectInput("comparison_age_range", 
-                  "Select an Age Range for Comparison:",
-                  choices = c("Babies < 1", "Children 1-6", "Children 7-11", 
-                              "Ages 12-20", "Adults 21-99")),
-      actionButton("compare_button", "Compare to Others"),
-      br()
+  tabsetPanel(
+    # Tab for TSH
+    tabPanel("TSH Level Distribution", 
+             sidebarLayout(
+               sidebarPanel(
+                 h3("Distribution of TSH Levels"),
+                 numericInput("tsh_value", 
+                              "Input TSH Value (in mU/L):", 
+                              value = 2, 
+                              min = 0, 
+                              max = 530, 
+                              step = 0.25),
+                 p("Input TSH value."),
+                 
+                 radioButtons("sex", 
+                              "Select Sex for Distribution:",
+                              choices = c("All", "M", "F"), 
+                              selected = "All"),
+                 p("Use the buttons to filter by sex."),
+                 
+                 br(),
+                 
+                 h3("Compare Your TSH Level to others and see if it's within the Expected Range"),
+                 numericInput("user_age", "Enter your age:", value = 30, min = 0, max = 120),
+                 numericInput("user_tsh", "Enter your TSH value:", value = 2, min = 0, max = 530),
+                 selectInput("comparison_age_range", 
+                             "Age Range for Comparison (TSH):",
+                             choices = c("Babies < 1", "Children 1-6", "Children 7-11", 
+                                         "Ages 12-20", "Adults 21-99")),
+                 actionButton("compare_button", "Compare to Others"),
+                 br()
+               ),
+               
+               mainPanel(
+                 textOutput("no_labs_available_message"),
+                 plotOutput("TSHhistogram"),
+                 textOutput("comparison_message")  
+               )
+             )
     ),
     
-    mainPanel(
-      textOutput("no_labs_available_message"),
-      plotOutput("TSHhistogram"),
-      textOutput("comparison_message")  
+    # Tab for T3
+    tabPanel("Free T3 Level Distribution", 
+             sidebarLayout(
+               sidebarPanel(
+                 h3("Distribution of Free T3 Levels"),
+                 numericInput("t3_value", 
+                              "Input Free T3 Value (in pg/mL):", 
+                              value = 2.5, 
+                              min = 0, 
+                              max = 20, 
+                              step = 0.1),
+                 p("Input Free T3 value."),
+                 
+                 radioButtons("sex_t3", 
+                              "Select Sex for Distribution:",
+                              choices = c("All", "M", "F"), 
+                              selected = "All"),
+                 p("Use the buttons to filter by sex."),
+                 
+                 br(),
+                 
+                 h3("Compare Your Free T3 Level to others and see if it's within the Expected Range"),
+                 numericInput("user_age_t3", "Enter your age:", value = 30, min = 0, max = 120, step = 1),
+                 numericInput("user_t3", "Enter your T3 value:", value = 2.8, min = 0, max = 20, step = 0.1),
+                 selectInput("comparison_age_range_t3", 
+                             "Age Range for Comparison (T3):",
+                             choices = c("Babies < 1 year", 
+                                         "Children 1 to 6 years", 
+                                         "Children 7 to 11 years", 
+                                         "Children 12 to 17 years", 
+                                         "Adults 18 to 99 years")),
+                 actionButton("compare_button_t3", "Compare to Others"),
+                 br()
+               ),
+               
+               mainPanel(
+                 textOutput("no_labs_available_message_t3"),
+                 plotOutput("T3histogram"),
+                 textOutput("comparison_message_t3")  
+               )
+             )
     )
   )
 )
